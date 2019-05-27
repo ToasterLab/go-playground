6a456f317f503480d1c450ba5267e5ad1b3afc43
Huey <leejinhuey@gmail.com>
2019-05-27 12:45:13 +0800

    first 3 examples

---
+++ 2-variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial variable"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+
+	g := 2
+	fmt.Println(g)
+
+	var h, i string = "10", "11"
+	fmt.Println(h, i)
+}
